c164cfcf7bec28f08666b1306fa1510913a0cdf1
Alexander37ss <alexanderpalazuelos37@gmail.com>
2024-04-29 17:01:40 -0700

    Imprimir tareas en pantalla

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ class Tarea{
 let Tareas = [];
 const main = document.querySelector(".main");
 const footer = document.querySelector(".footer");
+const li = document.createElement("li");
+const todolist = document.querySelector("#real");
+const completed = document.querySelector(".completed");
+const pending = document.querySelector("#pending");
+const editing = document.querySelector(".editing");
 const input = document.querySelector(".new-todo");
 actualizarTareas();
 
@@ -34,11 +39,20 @@ function nuevaTarea(){
     }
 }
 
-/* function actualizarTareas(){
+ function actualizarTareas(){
+     todolist.innerHTML = "";
     if(Tareas.length == 0){
         main.classList.add("hidden");
         footer.classList.add("hidden");
+    }else{
+        main.classList.remove("hidden");
+        footer.classList.remove("hidden");
+        Tareas.forEach(x => {
+            const pendingClonado = pending.cloneNode(true);
+            pendingClonado.querySelector("#label").textContent = x.title;
+            todolist.appendChild(pendingClonado);
+        });
     }
     input.value = "";
 }
- */
+ 
