vnoriegamoreno/laboratorio-mydayapp-js

Task #4 - When edit mode is enable, it should focus edit input field and users should be able to save or unsave changes

src/index.js
@@ -1,78 +1,35 @@
 import "./css/base.css";
+import { DOM, watchers } from "./js/utils";
 import {
-  mainEl,
-  footerEl,
-  taskEl,
-  newTodoInputEl,
-  todoEl,
-} from "./js/utils/dom-elements";
+  taskStatusHandler,
+  editTaskViewModeHandler,
+  editTaskTitleHandler,
+} from "./js/handlers/task.handlers";
 import newTaskItemFactory from "./js/factory/new-task-item.factory";
 
-const existTasksInTodoList = () => {
-  if (!taskEl.hasElements()) {
-    mainEl.hide();
-    footerEl.hide();
-  } else {
-    mainEl.show();
-    footerEl.show();
-  }
-};
-
-newTodoInputEl.getElement().addEventListener("keypress", (e) => {
+DOM.controllers.newTodoInput.getElement().addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    const taskTitle = newTodoInputEl.getValue();
+    const taskTitle = DOM.controllers.newTodoInput.getValue();
     if (taskTitle) {
       newTaskItemFactory.addNewTodoTaskItem(taskTitle);
-      newTodoInputEl.setValue("");
-      existTasksInTodoList();
+      DOM.controllers.newTodoInput.setValue("");
+      watchers.existTasksInTodoList();
     } else {
       console.error("You can not create a task with empty title!");
     }
   }
 });
 
-const handlePendingTaskStatus = (e, taskItemEl) => {
-  if (
-    e.target.tagName === "INPUT" &&
-    e.target.getAttribute("type") === "checkbox"
-  ) {
-    if (e.target.checked) {
-      e.target.setAttribute("checked", "");
-      taskItemEl.setAttribute("class", "completed");
-    } else {
-      e.target.removeAttribute("checked");
-      taskItemEl.removeAttribute("class");
-    }
-  }
-};
-
-let mouseClicks = 0;
-let timer = null;
-
-const handleEditTaskMode = (e, taskItemEl) => {
-  if (e.target.tagName === "LABEL") {
-    const checkboxEl = e.target.previousSibling.previousSibling;
-    mouseClicks++;
-    if (mouseClicks === 1) {
-      timer = setTimeout(() => {
-        mouseClicks = 0;
-      }, 500);
-    }
-    if (mouseClicks === 2 && !checkboxEl.checked) {
-      taskItemEl.removeAttribute("class");
-      taskItemEl.setAttribute("class", "editing");
-      mouseClicks = 0;
-      clearTimeout(timer);
-    }
-  }
-};
-
-todoEl.getElement().addEventListener("click", (e) => {
+DOM.controllers.todoList.getElement().addEventListener("click", (e) => {
   const taskItemEl = e.target.parentNode.parentNode;
-  handlePendingTaskStatus(e, taskItemEl);
-  handleEditTaskMode(e, taskItemEl);
+  taskStatusHandler(e, taskItemEl);
+  editTaskViewModeHandler(e, taskItemEl);
 });
 
-(function () {
-  existTasksInTodoList();
+DOM.controllers.todoList.getElement().addEventListener("keydown", (e) => {
+  editTaskTitleHandler(e);
+});
+
+(function main() {
+  watchers.existTasksInTodoList();
 })();

src/js/factory/new-task-item.factory.js
@@ -1,4 +1,4 @@
-import { todoEl } from "../utils/dom-elements";
+import { DOM } from "../utils";
 
 class NewTaskItemFactory {
   createCheckBoxElement() {
@@ -54,7 +54,7 @@ class NewTaskItemFactory {
     taskItemEl.appendChild(divWrapperEl);
     taskItemEl.appendChild(editInputEl);
 
-    todoEl.getElement().appendChild(taskItemEl);
+    DOM.controllers.todoList.getElement().appendChild(taskItemEl);
   }
 }
 

src/js/utils/index.js
@@ -1,0 +1,20 @@
+import {
+  mainElementController,
+  footerElementController,
+  taskElementController,
+  newTodoInputElementController,
+  todoElementController,
+} from "./dom-manipulation";
+import { existTasksInTodoList } from "./utils";
+
+export const DOM = {
+  controllers: {
+    main: mainElementController,
+    footer: footerElementController,
+    taskItem: taskElementController,
+    newTodoInput: newTodoInputElementController,
+    todoList: todoElementController,
+  },
+};
+
+export const watchers = { existTasksInTodoList };

src/js/utils/utils.js
@@ -1,0 +1,11 @@
+import { DOM } from "./";
+
+export const existTasksInTodoList = () => {
+  if (!DOM.controllers.taskItem.hasElements()) {
+    DOM.controllers.main.hide();
+    DOM.controllers.footer.hide();
+  } else {
+    DOM.controllers.main.show();
+    DOM.controllers.footer.show();
+  }
+};

platzi/laboratorio-mydayapp-vanillajs

src/index.js
@@ -1,78 +1,37 @@
 import "./css/base.css";
+import { DOM, watchers } from "./js/utils";
 import {
-  mainEl,
-  footerEl,
-  taskEl,
-  newTodoInputEl,
-  todoEl,
-} from "./js/utils/dom-elements";
+  taskStatusHandler,
+  editTaskViewModeHandler,
+  editTaskTitleHandler,
+} from "./js/handlers/task.handlers";
 import newTaskItemFactory from "./js/factory/new-task-item.factory";
 
-const existTasksInTodoList = () => {
-  if (!taskEl.hasElements()) {
-    mainEl.hide();
-    footerEl.hide();
-  } else {
-    mainEl.show();
-    footerEl.show();
-  }
-};
-
-newTodoInputEl.getElement().addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    const taskTitle = newTodoInputEl.getValue();
-    if (taskTitle) {
-      newTaskItemFactory.addNewTodoTaskItem(taskTitle);
-      newTodoInputEl.setValue("");
-      existTasksInTodoList();
-    } else {
-      console.error("You can not create a task with empty title!");
+DOM.controllers.newTodoInputEl
+  .getElement()
+  .addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      const taskTitle = DOM.controllers.newTodoInputEl.getValue();
+      if (taskTitle) {
+        newTaskItemFactory.addNewTodoTaskItem(taskTitle);
+        DOM.controllers.newTodoInputEl.setValue("");
+        watchers.existTasksInTodoList();
+      } else {
+        console.error("You can not create a task with empty title!");
+      }
     }
-  }
-});
+  });
 
-const handlePendingTaskStatus = (e, taskItemEl) => {
-  if (
-    e.target.tagName === "INPUT" &&
-    e.target.getAttribute("type") === "checkbox"
-  ) {
-    if (e.target.checked) {
-      e.target.setAttribute("checked", "");
-      taskItemEl.setAttribute("class", "completed");
-    } else {
-      e.target.removeAttribute("checked");
-      taskItemEl.removeAttribute("class");
-    }
-  }
-};
-
-let mouseClicks = 0;
-let timer = null;
-
-const handleEditTaskMode = (e, taskItemEl) => {
-  if (e.target.tagName === "LABEL") {
-    const checkboxEl = e.target.previousSibling.previousSibling;
-    mouseClicks++;
-    if (mouseClicks === 1) {
-      timer = setTimeout(() => {
-        mouseClicks = 0;
-      }, 500);
-    }
-    if (mouseClicks === 2 && !checkboxEl.checked) {
-      taskItemEl.removeAttribute("class");
-      taskItemEl.setAttribute("class", "editing");
-      mouseClicks = 0;
-      clearTimeout(timer);
-    }
-  }
-};
-
-todoEl.getElement().addEventListener("click", (e) => {
+DOM.controllers.todoListEl.getElement().addEventListener("click", (e) => {
   const taskItemEl = e.target.parentNode.parentNode;
-  handlePendingTaskStatus(e, taskItemEl);
-  handleEditTaskMode(e, taskItemEl);
+  taskStatusHandler(e, taskItemEl);
+  editTaskViewModeHandler(e, taskItemEl);
 });
 
-(function () {
-  existTasksInTodoList();
+DOM.controllers.todoListEl.getElement().addEventListener("keydown", (e) => {
+  editTaskTitleHandler(e);
+});
+
+(function main() {
+  watchers.existTasksInTodoList();
 })();

src/js/factory/new-task-item.factory.js
@@ -1,4 +1,4 @@
-import { todoEl } from "../utils/dom-elements";
+import { DOM } from "../utils";
 
 class NewTaskItemFactory {
   createCheckBoxElement() {
@@ -54,7 +54,7 @@ class NewTaskItemFactory {
     taskItemEl.appendChild(divWrapperEl);
     taskItemEl.appendChild(editInputEl);
 
-    todoEl.getElement().appendChild(taskItemEl);
+    DOM.controllers.todoListEl.getElement().appendChild(taskItemEl);
   }
 }
 

src/js/utils/index.js
@@ -1,0 +1,20 @@
+import {
+  mainElementController,
+  footerElementController,
+  taskElementController,
+  newTodoInputElementController,
+  todoElementController,
+} from "./dom-manipulation";
+import { existTasksInTodoList } from "./utils";
+
+export const DOM = {
+  controllers: {
+    mainEl: mainElementController,
+    footerEl: footerElementController,
+    taskItemEl: taskElementController,
+    newTodoInputEl: newTodoInputElementController,
+    todoListEl: todoElementController,
+  },
+};
+
+export const watchers = { existTasksInTodoList };

src/js/utils/utils.js
@@ -1,0 +1,11 @@
+import { DOM } from "./";
+
+export const existTasksInTodoList = () => {
+  if (!DOM.controllers.taskItemEl.hasElements()) {
+    DOM.controllers.mainEl.hide();
+    DOM.controllers.footerEl.hide();
+  } else {
+    DOM.controllers.mainEl.show();
+    DOM.controllers.footerEl.show();
+  }
+};