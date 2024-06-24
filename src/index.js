77a93f3ae3e602616d7feb8ff59523b420c4900d
Bryan <bsn9704@gmail.com>
2024-06-23 22:58:08 -0400

    feat: allow adding new tasks by typing on the input field

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import "./css/base.css";
 // DOM elements
 const mainElement = document.querySelector(".main");
 const footerElement = document.querySelector(".footer");
+const todoInput = document.querySelector(".new-todo");
+const todoList = document.querySelector(".todo-list");
 
 const tasks = [];
 
@@ -12,3 +14,82 @@ window.onload = () => {
     footerElement.hidden = true;
   }
 };
+
+todoInput.onkeypress = (e) => {
+  if (e.code !== "Enter") {
+    return;
+  }
+
+  addTask(e.target.value);
+  e.target.value = "";
+};
+
+function addTask(task) {
+  // make sure the string isn't empty
+  if (task.trim().length === 0) {
+    return;
+  }
+
+  tasks.push({
+    id: tasks.length + 1,
+    title: task.trim(),
+    completed: false,
+  });
+
+  updateTodoList();
+}
+
+function updateTodoList() {
+  // clear todo-list element
+  todoList.textContent = ``;
+
+  tasks.forEach((task) => {
+    // EXAMPLE
+    //
+    // <li data-task-id="1">
+    //   <div class="view">
+    //     <input class="toggle" type="checkbox" />
+    //     <label>Buy a unicorn</label>
+    //     <button class="destroy"></button>
+    //   </div>
+    //   <input class="edit" value="Buy a unicorn" />
+    // </li>
+
+    const li = document.createElement("li");
+    li.dataset.taskId = task.id;
+    if (task.completed) {
+      li.classList.add("completed");
+    }
+
+    const div = document.createElement("div");
+    div.classList.add("view");
+
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.checked = task.completed;
+    checkBox.classList.add("toggle");
+
+    const label = document.createElement("label");
+    label.textContent = task.title;
+
+    const button = document.createElement("button");
+    button.classList.add("destroy");
+
+    div.appendChild(checkBox);
+    div.appendChild(label);
+    div.appendChild(button);
+
+    const editInput = document.createElement("input");
+    editInput.value = task.title;
+    editInput.classList.add("edit");
+
+    li.appendChild(div);
+    li.appendChild(editInput);
+
+    todoList.appendChild(li);
+  });
+
+  const tasksEmpty = tasks === null || tasks.length === 0;
+  mainElement.hidden = tasksEmpty;
+  footerElement.hidden = tasksEmpty;
+}
